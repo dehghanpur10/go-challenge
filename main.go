45a35cbf39bca39d4d848f7b5b7493f535b81ef5
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-08-27 13:00:13 +0430

    set device router added

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"go-challenge/routes/deviceRoute"
+	"log"
+	"net/http"
+)
 
 func main(){
-	_ = mux.NewRouter()
+	router := mux.NewRouter()
+
+	deviceRoute.SetDeviceRouter(router)
+	http.Handle("/", router)
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Println(err)
+	}
 }
---
+++ routes/deviceRoute/main.go
@@ -0,0 +1,10 @@
+package deviceRoute
+
+import "github.com/gorilla/mux"
+
+
+//SetDeviceRouter is func that set router of device model
+func SetDeviceRouter(router *mux.Router) {
+	//router.HandleFunc("/devices").Methods("GET")
+	//router.HandleFunc("/devices/{id}").Methods("GET")
+}
