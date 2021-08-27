f8e820fdf39f73cdd869e4acb9971ae0ee326e99
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-08-27 13:05:55 +0430

    add device controller

---
+++ controller/deviceController/getDevice.go
@@ -0,0 +1,8 @@
+package deviceController
+
+import "net/http"
+
+//GetDevice is controller for get device from dynamoDB
+func GetDevice(w http.ResponseWriter, r *http.Request) {
+
+}
--- routes/deviceRoute/main.go
+++ routes/deviceRoute/main.go
@@ -1,10 +1,12 @@
 package deviceRoute
 
-import "github.com/gorilla/mux"
-
+import (
+	"github.com/gorilla/mux"
+	"go-challenge/controller/deviceController"
+)
 
 //SetDeviceRouter is func that set router of device model
 func SetDeviceRouter(router *mux.Router) {
-	//router.HandleFunc("/devices").Methods("GET")
-	//router.HandleFunc("/devices/{id}").Methods("GET")
+	router.HandleFunc("/devices", deviceController.GetDevice).Methods("GET")
+	router.HandleFunc("/devices/{id}", deviceController.SetDevice).Methods("POST")
 }
